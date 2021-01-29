3eb5133b87984cb6e70c3a29e2dd98680c4ef19f
Gerson Angel <gerson.angel@hotmail.es>
2021-01-28 19:47:51 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './characters/character/character.component';
+import { CharactersComponent } from './characters/characters.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { HeaderComponent } from './header/header.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharactersComponent,
+    FavouritesComponent,
+    HeaderComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
